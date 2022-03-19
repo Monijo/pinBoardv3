093e4b7404cdab06f1d1fe533c5d2db5b5c5b081
Monijo <mokajozefowicz@gmail.com>
2022-03-19 22:17:50 +0100

    Add forms.

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'pinBoard',
 ]
 
@@ -127,3 +129,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "pinBoard.User"
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
---
+++ pinBoard/forms.py
@@ -0,0 +1,95 @@
+import crispy_forms.bootstrap
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.forms import TextInput, NumberInput, PasswordInput, Textarea, DateTimeInput, TimeInput
+
+from pinBoard.models import ShopItem, Task, Note, Meeting
+
+
+class ShopItemForm(forms.ModelForm):
+    class Meta:
+        model = ShopItem
+        fields = ("name", "quantity")
+
+        widgets = {
+            'name': TextInput(attrs={'placeholder': "Nazwa przedmiotu do kupienia"}),
+            'quantity': NumberInput(attrs={'placeholder': "Ilość"})
+        }
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ("content", "priority")
+
+        widgets = {
+            'content': TextInput(attrs={'placeholder': "treść zadania"})
+
+        }
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password', 'email')
+
+        error_messages = {
+            'username': {
+                'unique': ("Podany login jest zajęty!")
+            }
+        }
+
+        widgets = {
+            'username': TextInput(attrs={'placeholder': 'Nazwa użytkownika'}),
+            'password': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'twoje hasło'
+            }),
+            'email': TextInput(attrs={'placeholder': "Adres e-mail"})
+        }
+
+    def save(self, commit=True, *args):
+        user = super().save(commit=False)
+        user.password = make_password(self.cleaned_data.get('password'))
+        user.username = self.cleaned_data.get('username')
+
+        if commit:
+            user.save()
+        return user
+
+
+class UserLogInForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password')
+
+        widgets = {
+            'username': TextInput(attrs={'placeholder': 'Nazwa użytkownika'}),
+            'password': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'twoje hasło'
+            })
+
+        }
+
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ('content',)
+
+        widgets = {
+            'content': Textarea(attrs={'placeholder': 'treść notatki'})
+        }
+
+
+class MeetingForm(forms.ModelForm):
+    class Meta:
+        model = Meeting
+        fields = ("date", "hour", "location", "name")
+
+        widgets = {
+            "date": DateTimeInput(attrs={'placeholder': "rrrr-mm-dd"}),
+            "hour": TimeInput(attrs={'placeholder': 'hh:mm:ss'})
+        }
