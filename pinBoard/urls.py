056bc3a59b834f0b3b25a0d39c80ea5068f4b001
Monijo <mokajozefowicz@gmail.com>
2022-03-19 19:37:28 +0100

    Add urls.

---
+++ pinBoard/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'pinBoard'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('sign-up', views.sign_up, name='sign_up'),
+    path('log', views.sign_in, name='sign_in'),
+    path('logout', views.log_out, name='log_out'),
+
+    path('users/', views.dashboard, name='dashboard'),
+    path('users/shop_list_form', views.shop_list_form, name='shop_list_form'),
+    path('users/task-form', views.task_form, name='task_form'),
+    path('users/archive', views.archive, name='archive'),
+    path('users/sensors', views.sensors, name='sensors'),
+
+    path('users/<int:id>', views.member_view, name='member_view'),
+    path('users/<int:id>/note_form', views.note_form, name='note_form'),
+    path('users/<int:id>/all_notes', views.all_notes, name='all_notes'),
+    path('users/<int:id>/meeting_form', views.meeting_form, name='meeting_form'),
+    path('users/<int:id>/all_meetings', views.all_meetings, name='all_meetings'),
+
+]
--- pinBoard/views.py
+++ pinBoard/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    pass
+
+
+def sign_up(request):
+    pass
+
+
+def sign_in(request):
+    pass
+
+
+def log_out(request):
+    pass
+
+
+def dashboard(request):
+    pass
+
+
+def shop_list_form(request):
+    pass
+
+
+def task_form(request):
+    pass
+
+
+def archive(request):
+    pass
+
+
+def sensors(request):
+    pass
+
+
+def member_view(request):
+    pass
+
+
+def note_form(request):
+    pass
+
+
+def all_notes(request):
+    pass
+
+
+def meeting_form(request):
+    pass
+
+
+def all_meetings(request):
+    pass
